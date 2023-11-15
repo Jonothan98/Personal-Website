b1ed9073e6a568a8dd42de88259280e8bde63f1f
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-11-15 13:18:46 -0600

    app: created landing page component. Removed App.css and index.css

---
+++ app/src/Components/LandingPage.js
@@ -0,0 +1,13 @@
+
+function LandingPage() {
+  return (
+    <div>
+      <h1>Landing Page</h1>
+      <header>
+        HelloWorld
+      </header>
+    </div>
+  );
+}
+
+export default LandingPage;
