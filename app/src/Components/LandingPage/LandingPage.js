ee04c2a6bc3e1bab5a07830abc5cff5fe51686f4
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-11-16 15:34:09 -0600

    app: add name, position and about me text. Created landingPage css file and used flexbox to center left the text on the landing page.

---
+++ app/src/Components/LandingPage/LandingPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css'
+
+function LandingPage() {
+  return (
+    <div className='landing-page-container'>
+      <div className='text-container'>
+        <h1 className='name'>Jonothan Castillo</h1>
+        <h2 className='position'>Full Stack Software Developer</h2>
+        <p className='about-me'>I am a Software Developer with experience in full-stack development using React, Java, and MySQL. I have experience using various agile practices with a team like Scrum and test-driven development. I like contributing to a team and I am eager to learn new skills.</p>
+      </div>
+    </div>
+  );
+}
+
+export default LandingPage;
--- app/src/index.js
+++ app/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import LandingPage from './Components/LandingPage';
+import LandingPage from './Components/LandingPage/LandingPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
