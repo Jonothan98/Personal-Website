a5ffa1fa6082550b941ce16bfc911bd8105317ec
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-11-28 16:56:44 -0600

    app: created projects page component and added the route to app.js

--- app/src/app.js
+++ app/src/app.js
@@ -3,15 +3,17 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './Components/LandingPage/LandingPage';
 import Header from './Components/Header/Header';
 import AboutMe from './Components/AboutMe/AboutMe';
+import Projects from './Components/Projects/Projects';
 import "bootstrap/dist/css/bootstrap.min.css"
 
 function app() {
     return (
         <BrowserRouter>
-            <Header/>
+            <Header />
             <Routes>
                 <Route path="*" element={<LandingPage />}></Route>
-                <Route path="/about-me" element={<AboutMe/>}></Route>
+                <Route path="/about-me" element={<AboutMe />}></Route>
+                <Route path='/projects' element={<Projects />}></Route>
             </Routes>
         </BrowserRouter>
     )
