663c2cb3ba1beb8cfca37cc09332208e53ed64b5
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-11-30 14:32:27 -0600

    app: added h1 project title to page, limited the width of the page body to 60% of the page.

--- app/src/Components/Projects/Projects.js
+++ app/src/Components/Projects/Projects.js
@@ -5,12 +5,15 @@ import "./Projects.css";
 function Projects() {
   return (
     <div className='projects-container'>
+      <div className='projects-title'>
+        <h1>Projects</h1>
+      </div>
       <div className='project-card-container'>
         <div className='image-container'>
           <img className="card-image" src='https://img.itch.zone/aW1hZ2UvNTA3MDIwLzI3MTkzNzQuanBn/347x500/TBn0w1.jpg' alt='Journey Down thumbnail'></img>
         </div>
         <div className='card-text'>
-          <h1>Journey Down</h1>
+          <h2>Journey Down</h2>
           <p>
             The goal for Journey Down was to make a easy RPG mobile game with the focus of one hand play and randomized loot. <br></br><br></br>
             I worked on the combat system for this while my partner Marvin Jung worked on the randomized loot system. <br></br><br></br>
@@ -27,7 +30,7 @@ function Projects() {
           <img className="card-image" src='https://img.itch.zone/aW1nLzMzMTY5MTYucG5n/original/mN%2BHH0.png' alt='Journey Down thumbnail'></img>
         </div>
         <div className='card-text'>
-          <h1>Terror in Herrington</h1>
+          <h2>Terror in Herrington</h2>
           <p>
             The goal for Terror in Herrington was to make a top down adventure game with the focus of exploring. <br></br><br></br>
             This was my second project in Unity and worked in a team of 3. I did the programming for this project. Another did the art and the last member was the leader in charge of helping some with both art and programming and keeping us on schedule. <br></br><br></br>
