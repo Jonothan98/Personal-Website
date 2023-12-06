adba15cb688a0e34cea5ea20d74d7b3b2ceb527c
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-12-06 13:07:44 -0600

    app: changed fonts of all components to match landing page font. Made the landing page text to be centered and 60% width

--- app/src/Components/LandingPage/LandingPage.jsx
+++ app/src/Components/LandingPage/LandingPage.jsx
@@ -7,10 +7,12 @@ function LandingPage() {
       <div className='text-container'>
         <h1 className='name'>Jonothan Castillo</h1>
         <h2 className='position'>Full Stack Software Developer</h2>
-        <p className='about-me'>I am a Software Developer with experience in full-stack development using React, Java, and MySQL. I have experience using various agile practices with a team like Scrum and test-driven development. I like contributing to a team and I am eager to learn new skills.</p>
+        <p className='about-me'> I am a Software Developer with experience in full-stack development using React, Java, and MySQL.</p>
       </div>
     </div>
   );
 }
 
 export default LandingPage;
+
+// I am a Software Developer with experience in full-stack development using React, Java, and MySQL. I have experience using various agile practices with a team like Scrum and test-driven development. I like contributing to a team and I am eager to learn new skills.
