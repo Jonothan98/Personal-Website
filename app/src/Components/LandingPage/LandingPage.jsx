d6f835c439621595a20af450e0910efd58406ee9
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-12-06 13:28:11 -0600

    app: made changes to the landing page text

--- app/src/Components/LandingPage/LandingPage.jsx
+++ app/src/Components/LandingPage/LandingPage.jsx
@@ -7,7 +7,7 @@ function LandingPage() {
       <div className='text-container'>
         <h1 className='name'>Jonothan Castillo</h1>
         <h2 className='position'>Full-Stack Software Developer</h2>
-        <p className='about-me'> I am a full-stack software developer with experience in using React, Java, and MySQL.</p>
+        <p className='about-me'> I am a full-stack software developer with experience in using React, Java, and MySQL. I like the problem-solving element of development, and look forward to working with others and learning more.</p>
       </div>
     </div>
   );
