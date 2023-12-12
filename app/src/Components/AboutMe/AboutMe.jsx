631da70810f6d4f3a45f7e6d8025a4587b5d8647
Jonothan Castillo <jonothan.x.castillo@gmail.com>
2023-12-12 12:05:34 -0600

    app: added resume button to landing page, and update about me text

--- app/src/Components/AboutMe/AboutMe.jsx
+++ app/src/Components/AboutMe/AboutMe.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
-import resume from "./Resume Jonothan Castillo.pdf"
+import resume from "../../Resume Jonothan Castillo.pdf"
 import "./AboutMe.css";
 
 function AboutMe() {
--- app/src/Components/LandingPage/LandingPage.jsx
+++ app/src/Components/LandingPage/LandingPage.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import Button from 'react-bootstrap/Button';
 import './LandingPage.css'
+import resume from "../../Resume Jonothan Castillo.pdf"
 
 function LandingPage() {
   return (
@@ -7,7 +9,14 @@ function LandingPage() {
       <div className='text-container'>
         <h1 className='name'>Jonothan Castillo</h1>
         <h2 className='position'>Full-Stack Software Developer</h2>
-        <p className='about-me'> I am a full-stack software developer with experience in using React, Java, and MySQL. I like the problem-solving element of development, and look forward to working with others and learning more.</p>
+        <p className='about-me'>
+          Graduate of the University of Houston
+          <br></br> A Full-stack software developer with experience in using React, Java, and MySQL.
+          <br></br> Problem-solver, Team Player, Adaptable, Proactive, and Open-Minded in learning new skills.
+        </p>
+        <div className='resume-button'>
+            <Button href={resume} target='_blank' rel='noreferrer'>Resume</Button>
+          </div>
       </div>
     </div>
   );
